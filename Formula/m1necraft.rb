f178c8b7c7b9b1e13608b3576493ffc2ab0210d3
Raphael Tang <raphpb1912@gmail.com>
2022-01-26 00:28:13 +0800

    (m1necraft): updated sha256 checksum

--- Formula/m1necraft.rb
+++ Formula/m1necraft.rb
@@ -2,7 +2,7 @@ class M1necraft < Formula
   desc "Patch Minecraft launcher to run Minecraft natively on ARM"
   homepage "https://raph.codes/projects/m1necraft"
   url "https://github.com/raphtlw/m1necraft/archive/v0.1.1.tar.gz"
-  sha256 "b3d9cbcf875d9b3a308b37673f23cd0b869f10619328e956b55d95102acb2d58"
+  sha256 "8976f9baa2e792b747d214320b58aae4223b6770572bfb8129fcee9178fb38dd"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
