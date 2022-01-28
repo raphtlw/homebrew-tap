42b61e467df8daff05006336d0b5c09861a97ae8
Raphael Tang <raphpb1912@gmail.com>
2022-01-28 23:51:25 +0800

    (m1necraft): version bump

--- Formula/m1necraft.rb
+++ Formula/m1necraft.rb
@@ -1,7 +1,7 @@
 class M1necraft < Formula
   desc "Patch Minecraft launcher to run Minecraft natively on ARM"
   homepage "https://raph.codes/projects/m1necraft"
-  url "https://github.com/raphtlw/m1necraft/archive/v0.1.1.tar.gz"
+  url "https://github.com/raphtlw/m1necraft/archive/v0.1.2.tar.gz"
   sha256 "8976f9baa2e792b747d214320b58aae4223b6770572bfb8129fcee9178fb38dd"
   license "GPL-3.0-only"
 
