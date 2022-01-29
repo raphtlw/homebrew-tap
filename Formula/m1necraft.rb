50020bb4282e79872a38c16db0aef377dcb5e778
raphtlw <47694127+raphtlw@users.noreply.github.com>
2022-01-29 10:43:19 +0000

    m1necraft 0.1.4

--- Formula/m1necraft.rb
+++ Formula/m1necraft.rb
@@ -1,8 +1,8 @@
 class M1necraft < Formula
   desc "Patch Minecraft launcher to run Minecraft natively on ARM"
   homepage "https://raph.codes/projects/m1necraft"
-  url "https://github.com/raphtlw/m1necraft/archive/v0.1.3.tar.gz"
-  sha256 "598c01e288a0ba77d4125ecaac7afcb4236c5e08b7665bbeb9ea1110f3375b8d"
+  url "https://github.com/raphtlw/m1necraft/archive/v0.1.4.tar.gz"
+  sha256 "e444ad1d9031e3de89d3b10dd4d69d6662da769502e5787d12fe7a54cc010203"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
