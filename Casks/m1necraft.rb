22ff7578e82134211f1496d89666fd41006129e6
Raphael Tang <raphpb1912@gmail.com>
2022-02-12 02:22:58 +0800

    (m1necraft cask): update cask

--- Casks/m1necraft.rb
+++ Casks/m1necraft.rb
@@ -1,13 +1,16 @@
 cask "m1necraft" do
-  version "0.2.0"
+  version "0.2.1"
   sha256 "f98eeda3518b7a7e1d31ac1a7d2b2773e669c3bbe464551dc2543c2ef3f277b7"
   
-  url "https://github.com/raphtlw/m1necraft/releases/latest/download/M1necraft.app.zip"
+  url "https://raw.githubusercontent.com/raphtlw/m1necraft/main/installer/install.sh"
   name "M1necraft"
   desc "Patch Minecraft launcher to run Minecraft natively on ARM"
   homepage "https://raph.codes/projects/m1necraft"
   
   app "M1necraft.app"
+  
+  installer script: "#{staged_path}/install.sh"
+  uninstall trash: "/Applications/M1necraft.app"
   
   zap trash: [
     '~/Library/Application Support/raphtlw.apps.M1necraft'
