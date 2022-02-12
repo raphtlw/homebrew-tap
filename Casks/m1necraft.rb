1f5e5cdccf2a1d8f0791c4eb70540863bcff9d50
runner <runner@Mac-1644641560706.local>
2022-02-12 04:56:06 +0000

    Update m1necraft from 0.2.1 to v0.2.2

--- Casks/m1necraft.rb
+++ Casks/m1necraft.rb
@@ -1,19 +1,19 @@
 cask "m1necraft" do
-  version "0.2.1"
+  version "v0.2.2"
   sha256 :no_check
-  
+
   url "https://raw.githubusercontent.com/raphtlw/m1necraft/main/installer/install.sh",
       verified: "raw.githubusercontent.com/raphtlw/m1necraft/"
   name "M1necraft"
   desc "Patch Minecraft launcher to run Minecraft natively on ARM"
   homepage "https://raph.codes/projects/m1necraft"
-  
+
   app "M1necraft.app"
-  
   installer script: "#{staged_path}/install.sh"
+
   uninstall trash: "/Applications/M1necraft.app"
-  
+
   zap trash: [
-    '~/Library/Application Support/raphtlw.apps.M1necraft'
+    "~/Library/Application Support/raphtlw.apps.M1necraft",
   ]
 end
